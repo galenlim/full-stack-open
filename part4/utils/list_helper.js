d2374f860e1ed5eb5ce3dd2a725a0f482874a5a4
Galen Lim <jingliang.lim@gmail.com>
2021-07-13 16:15:59 +0800

    Ex 4.4 and 4.5 - Add and test helper functions totalLikes and favoriteBlogs

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,33 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes)
+
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    return likes.reduce(reducer, 0)
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    } else {
+        const sortedByLikes = blogs.sort((first, second) => {
+            return second.likes - first.likes
+        })
+
+        const result = (({title, author, likes}) => ({title, author, likes}))(sortedByLikes[0])
+
+        return result
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
 
