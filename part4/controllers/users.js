a91fd6cdec18fa802daf7f19143e1c0e71b20c4d
Galen Lim <jingliang.lim@gmail.com>
2021-07-27 17:10:23 +0800

    Ex 4.17: Add user ref to blog and vice versa

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -36,7 +36,7 @@ usersRouter.post('/', async (request, response) => {
 
 usersRouter.get('/', async (request, response) => {
     const users = await User
-        .find({})
+        .find({}).populate('blogs')
 
     response.json(users)
 })
