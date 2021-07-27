ad402fdd02b8943c5ebd03e435dbb3e28a3a269b
Galen Lim <jingliang.lim@gmail.com>
2021-07-27 21:42:41 +0800

    Ex 4.18 - Implement token-based authentication

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -8,10 +9,24 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
     const body = request.body
 
-    const user = await User.findById(body.userId)
+    const user = await User.findById(decodedToken.id)
 
     body.user = user._id
 
