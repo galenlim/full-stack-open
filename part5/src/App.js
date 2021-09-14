903953cf9a0e745316c7f042b86f21eab50a9bbf
Galen Lim <jingliang.lim@gmail.com>
2021-09-14 12:13:39 +0800

    Ex 5.9 - Modify blog listing to sort by number of likes

--- part5/src/App.js
+++ part5/src/App.js
@@ -90,8 +90,10 @@ const App = () => {
           <CreateBlogForm createBlog={createBlog} /> 
         </div>
       </Togglable>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} />
       )}
     </div>
   )
