856dcccc9611fdcaa223e7dcbb0a7727dc55ac0f
Galen Lim <jingliang.lim@gmail.com>
2021-12-13 12:33:26 +0800

    Ex 8.4 - Implement optional author parameter for allBooks query

--- part8/library-backend.js
+++ part8/library-backend.js
@@ -104,16 +104,21 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!,
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
-console.log(books.length)
 const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      } else {
+        return books
+      }
+    },
     allAuthors: () => authors
   },
   Author: {
