2ad478e8ece5cc0afe0f54bf80a250505ea8ffd5
Galen Lim <jingliang.lim@gmail.com>
2021-12-29 16:49:07 +0800

    Ex 8.19 - Add filtering of book list by genre

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('All Genres')
   const result = useQuery(ALL_BOOKS)
 
   if (result.loading) return <div>loading</div>
@@ -12,11 +13,12 @@ const Books = (props) => {
   }
 
   const books = result.data.allBooks
+  const genres = books.map(b => b.genres).flat().filter((b, i, a) => a.indexOf(b) === i).concat('All Genres')
 
   return (
     <div>
       <h2>books</h2>
-
+      in genre <strong>{genre}</strong>
       <table>
         <tbody>
           <tr>
@@ -28,15 +30,18 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
+          {books
+              .filter(b => genre === 'All Genres' ? true : b.genres.includes(genre))
+              .map(b =>
+                <tr key={b.title}>
+                  <td>{b.title}</td>
+                  <td>{b.author.name}</td>
+                  <td>{b.published}</td>
+                </tr>
           )}
         </tbody>
       </table>
+      {genres.map(g => <button key={g} value={g} onClick={(e) => setGenre(e.target.value)}>{g}</button>)}
     </div>
   )
 }
